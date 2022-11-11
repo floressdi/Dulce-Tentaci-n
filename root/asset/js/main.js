cc53e33eddda04cac9a8b0824a4628e74488d88e
Diego Flores <del.angelfull@gmail.com>
2022-11-10 18:17:07 -0600

    V1.3

--- root/asset/js/main.js
+++ root/asset/js/main.js
@@ -1,8 +1,20 @@
 // Show Menu 😎
 let btnMenu = document.getElementById('bx-menu');
 let showMenu = document.getElementById('menu-burger');
+const btnLogin = document.getElementById('btn-burger-login');
+const showLogin = document.getElementById('login');
+const btnBack = document.getElementById('btn-back');
+
 
 btnMenu.addEventListener('click', ()=>{
     showMenu.classList.toggle('show-menu');
     btnMenu.classList.toggle('bx-x')
+})
+
+btnLogin.addEventListener('click',()=>{
+    showLogin.classList.toggle('show-login');
+})
+
+btnBack.addEventListener('click',()=>{
+    showLogin.classList.toggle('show-login');
 })
